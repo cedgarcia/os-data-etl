281d7facbf4b727a2f6f77ad45d3741dbffa84bc
Cedrick Garcia <cedrick@dreamboat-labs.com>
2025-10-15 10:32:40 +0800

    update mappings

--- data/mappings.js
+++ data/mappings.js
@@ -17,12 +17,12 @@ export const categoryMap = {
   //old(from mssql) : new(webiny)
   // 68ecc79a09c7f100022f6b5f#0001 // PHOTOS
   // 2: '68b51b6592a5a100023bd2e9#0001', // NEWS
-  2: '68ecc802f7ad160002f0fafa#0001', // NEWS
+  2: '68ecc802f7ad160002f0fafa#0004', // NEWS
 
   16: '68dccac3b59a9e0002210b56#0017', // EDITORIAL
-  17: '68ecc7e659b6ba000244c0c3#0001', // FEATURES
-  18: '68ecc7c309c7f100022f6b60#0001', // SPORTS LIFE
-  19: '68ecc77d09c7f100022f6b5e#0001', // VIDEOS
+  17: '68ecc7e659b6ba000244c0c3#0003', // FEATURES
+  18: '68ecc7c309c7f100022f6b60#0003', // SPORTS LIFE
+  19: '68ecc77d09c7f100022f6b5e#0003', // VIDEOS
 }
 // mapping old MSSQL league IDs to Webiny IDs
 export const leagueMap = {
@@ -39,14 +39,14 @@ export const leagueMap = {
    */
 
   //old(from mssql) : new(webiny)
-  6: '68ecc928aa1fd0000294934e#0001', // GILAS
-  7: '68ecc834d079bb0002731b3a#0002', // MORE SPORTS
-  9: '68ecc947aa1fd0000294934f#0001 ', // PBA
-  10: '68ecc906aa1fd0000294934d#0001', // PVL
-  11: '68ecc89ad079bb0002731b3b#0002', // UAAP
+  6: '68ecc928aa1fd0000294934e#0005', // GILAS
+  7: '68ecc834d079bb0002731b3a#0006', // MORE SPORTS
+  9: '68ecc947aa1fd0000294934f#0005', // PBA
+  10: '68ecc906aa1fd0000294934d#0005', // PVL
+  11: '68ecc89ad079bb0002731b3b#0008', // UAAP
   16: '68dccc3de7e1750002cec96a#0023', // ESPORTS
-  21: '68e778d30b95de0002ec1c8d#0004', // PARIS 2024
-  22: '68ec8f774d569300022ee5e5#0002', // ALAS
+  21: '68edb20494124e00022ee827#0005', // PARIS 2024
+  22: '68edb21fb7f1a40002ab037a#0005', // ALAS
 
 
 }
