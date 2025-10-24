9b8895e9bef0df73f820e63647520e7f1a5fabbf
Cedrick Garcia <icedgarcia@gmail.com>
2025-10-24 17:47:25 +0800

    revert

--- utils/cleanArticleBody.js
+++ utils/cleanArticleBody.js
@@ -1,21 +1,11 @@
-// cleanArticleBody.js
 import * as cheerio from 'cheerio'
 import { decode } from 'html-entities'
 
 export const cleanArticleBody = (htmlContent) => {
-  // Handle cases where htmlContent is not a string or is empty
-  if (!htmlContent || typeof htmlContent !== 'string') {
-    return '<p></p>'
-  }
-
   const decoded = decode(htmlContent)
 
-  // Load the decoded content into Cheerio
-  const $ = cheerio.load(decoded, { decodeEntities: false })
+  // const $ = cheerio.load(decoded)
+  const $ = cheerio.load(decoded, null, false)
 
-  // Wrap the content in a <p> tag
-  $('body').children().wrapAll('<p></p>')
-
-  // Return the HTML content, ensuring it's wrapped in a single <p> tag
-  return $('body').html() || '<p></p>'
+  return $.html()
 }
