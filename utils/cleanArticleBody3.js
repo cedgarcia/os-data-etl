dbf673003c39f3cc9a9ffb0a39ddb85a5364d3ae
Cedrick Garcia <icedgarcia@gmail.com>
2025-11-13 15:01:38 +0800

    important

---
+++ utils/cleanArticleBody3.js
@@ -0,0 +1,72 @@
+// cleanHtmlToBlocks.js
+import * as cheerio from 'cheerio'
+import { nanoid } from 'nanoid'
+
+export default (html) => {
+  const $ = cheerio.load(html)
+  const contentBlocks = []
+
+  $('body')
+    .children()
+    .each((_, el) => {
+      let key = ''
+      let data = ''
+      let element = el
+      const children = $(element).children()
+      if (children.length) {
+        element = children[0]
+      }
+
+      const tag = element.children[0]?.tagName ?? 'p'
+
+      switch (tag.toLowerCase()) {
+        case 'h1':
+          key = 'heading'
+          data = $(element).html().trim()
+          break
+        case 'p':
+        case 'div':
+        case 'span':
+          key = 'paragraph'
+          data = $(element).html().trim()
+          break
+        case 'iframe':
+        case 'img':
+        case 'video':
+          key = 'embed_html'
+          data = $.html(element).trim()
+          break
+        default:
+          return
+      }
+
+      if (data && data.trim()) {
+        contentBlocks.push({
+          id: nanoid(),
+          key,
+          data,
+        })
+      }
+    })
+
+  let body = ''
+  if (contentBlocks.length) {
+    const tagMapping = {
+      paragraph: 'p',
+      heading: 'h1',
+      embed_html: 'div',
+    }
+
+    body = contentBlocks
+      .map((block) => {
+        const tag = tagMapping[block.key]
+        return `<${tag}>${block.data}</${tag}>`
+      })
+      .join('')
+  }
+
+  return {
+    body,
+    contentBlocks,
+  }
+}
